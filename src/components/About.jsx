b49baa26956576f6a0eaaa30cdad1e1dca3d1d22
Dharmi <dharmip060@gmail.com>
2023-12-28 17:18:50 +0530

    new launch added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,13 +12,13 @@ const About = () => {
         </div>
 
         <div className="justify-center md:justify-end mt-4">
-          <p className="text-white text-lg max-w-lg font-sans">
+          <p className="text-white text-lg max-w-lg font-sans m-4">
           iNav Labs pioneers robotics and AI, seamlessly integrating precision and intelligence. Our excellence in autonomous systems, machine learning, and human-robot interaction defines the future through code and mechanical mastery. Welcome to iNav Labs.
           </p>
           {/* You can uncomment the following line if you want to include a subscribe link */}
           {/* <a href="#subscribe" className="underline font-serif text-white">Subscribe</a> */}
           <div className='pt-3  '>
-          <a href="#subscribe" className='text-white border-primary border-2 p-1 rounded-lg text-sm  hover:shadow-md hover:shadow-primary transform transition-transform hover:scale-105 '>Subscribe</a>
+          <a href="#subscribe" className='text-white border-primary border-2 p-1 rounded-lg text-sm  hover:shadow-md hover:shadow-primary transform transition-transform hover:scale-105 m-4 '>Subscribe</a>
         </div>
         </div>
       </div>
